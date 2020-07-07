9e4119bbce63faa63255b252981e82932cbf41a9
zhukaiyuan <zhuky9527@qq.com>
2020-07-07 21:53:11 +0800

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,12 +4,27 @@ module.exports = {
   markdown: {
     lineNumbers: true
   },
+  searchMaxSuggestions: 10,
+  lastUpdated: '最后更新时间',
+  repo: 'yookoo/yookoo.github.io',
+  repoLabel: '查看源码',
+  docsDir: 'docs',
+  // 默认是 false, 设置为 true 来启用
+  editLinks: true,
+  // 默认为 "Edit this page"
+  editLinkText: '帮助我们改善此页面！',
+  smoothScroll: true,
   themeConfig: {
       logo: '/logo.png',
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '教程', link: '/guide/' },
+        { text: 'rising-waves', link: 'https://yookoo.github.io/rising-waves/#/' },
+      ]
     },
   plugins: {
       "vuepress-plugin-auto-sidebar": {},
-      '@vuepress/active-header-links': true,
-       "@vuepress/back-to-top": true
+      "@vuepress/active-header-links": true,
+      "@vuepress/back-to-top": true
     }
 }
